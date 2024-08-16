b41f3ffc910a01bbf37e77398b712e9eb876aedf
NotJerwee <jerrylin2488@gmail.com>
2024-08-16 15:07:48 -0400

    Update page.js

--- app/generate/page.js
+++ app/generate/page.js
@@ -144,22 +144,33 @@ export default function Generate() {
 
 			{flashcards.length > 0 && (
 				<Container maxWidth="md">
-					<Grid container spacing={3} sx={{ mt: 4 }}>
-						{flashcards.map((flashcard, index) => (
-							<Grid item xs={12} sm={6} md={4} key={index}>
-								<Card>
-									<CardActionArea onClick={() => handleCardClick(flashcard.name)}>
-										<CardContent>
-											<Typography variant="h5" component="div">
-												{flashcard.name}
-											</Typography>
-										</CardContent>
-									</CardActionArea>
-								</Card>
-							</Grid>
-						))}
+				<Grid container spacing={3} sx={{ mt: 4 }}>
+				  {flashcards.map((flashcard) => (
+					<Grid item xs={12} sm={6} md={4} key={flashcard.id}>
+					  <Card>
+						<CardActionArea onClick={() => handleCardClick(flashcard.id)}>
+						  <CardContent>
+							<Box sx={{ /* Styling for flip animation */ }}>
+							  <div>
+								<div>
+									<Typography variant="h5" component="div">
+										{flashcard.front}
+								  	</Typography>
+								</div>
+								<div>
+									<Typography variant="h5" component="div">
+										{flashcard.back}
+								  	</Typography>
+								</div>
+							  </div>
+							</Box>
+						  </CardContent>
+						</CardActionArea>
+					  </Card>
 					</Grid>
-				</Container>
+				  ))}
+				</Grid>
+			  </Container>
 			)}
 
 			<Box sx={{ mt: 4, display: 'flex', justifyContent: 'center' }}>
