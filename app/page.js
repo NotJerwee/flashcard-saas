cf0f5c42a1c5f3d128417e87631b94eda2f9592e
NotJerwee <jerrylin2488@gmail.com>
2024-08-16 15:25:56 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,19 +1,86 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 
-export default function Home() {
+const FlashcardSaaS = () => {
+
+  const handleSubmit = async () => {
+	try {
+	  const checkoutSession = await fetch('/api/checkout_sessions', {
+		method: 'POST',
+		headers: { origin: 'http://localhost:3000' },
+	  });
+	  const checkoutSessionJson = await checkoutSession.json();
+
+	  const stripe = await getStripe();
+	  const { error } = await stripe.redirectToCheckout({
+		sessionId: checkoutSessionJson.id,
+	  });
+
+	  if (error) {
+		console.warn(error.message);
+	  }
+	} catch (err) {
+	  console.error('Error creating checkout session:', err);
+	}
+  };
+
   return (
-	<AppBar position="static">
-	  <Toolbar>
-		<Typography variant="h6" style={{flexGrow: 1}}>
-		  Flashcard SaaS
+	<div>
+	  <AppBar position="static">
+		<Toolbar>
+		  <Typography variant="h6" style={{ flexGrow: 1 }}>
+			Flashcard SaaS
+		  </Typography>
+		  <SignedOut>
+			<Button color="inherit" href="/sign-in">Login</Button>
+			<Button color="inherit" href="/sign-up">Sign Up</Button>
+		  </SignedOut>
+		  <SignedIn>
+			<UserButton />
+		  </SignedIn>
+		</Toolbar>
+	  </AppBar>
+
+	  <Box sx={{ textAlign: 'center', my: 4 }}>
+		<Typography variant="h2" component="h1" gutterBottom>
+		  Welcome to Flashcard SaaS
 		</Typography>
-		<SignedOut>
-		  <Button color="inherit" href="/sign-in">Login</Button>
-		  <Button color="inherit" href="/sign-up">Sign Up</Button>
-		</SignedOut>
-		<SignedIn>
-		  <UserButton />
-		</SignedIn>
-	  </Toolbar>
-	</AppBar>
+		<Typography variant="h5" component="h2" gutterBottom>
+		  The easiest way to create flashcards from your text.
+		</Typography>
+		<Button variant="contained" color="primary" sx={{ mt: 2, mr: 2 }} href="/generate">
+		  Get Started
+		</Button>
+		<Button variant="outlined" color="primary" sx={{ mt: 2 }}>
+		  Learn More
+		</Button>
+	  </Box>
+
+	  <Box sx={{ my: 6 }}>
+		<Typography variant="h4" component="h2" gutterBottom>Features</Typography>
+		<Grid container spacing={4}>
+		  {/* Feature items */}
+		</Grid>
+	  </Box>
+
+	  <Box sx={{ my: 6, textAlign: 'center' }}>
+		<Typography variant="h4" component="h2" gutterBottom>Pricing</Typography>
+		<Grid container spacing={4} justifyContent="center">
+		  {/* Pricing plans */}
+		</Grid>
+	  </Box>
+
+	  {/* Example of how you might use the handleSubmit function */}
+	  <Button variant="contained" color="primary" onClick={handleSubmit}>
+		Purchase Now
+	  </Button>
+	</div>
   );
-}
+};
+
+export default FlashcardSaaS;
