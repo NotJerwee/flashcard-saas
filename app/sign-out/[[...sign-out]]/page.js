51ce39bd1221d691104e574ce0a36f718bf19128
NotJerwee <jerrylin2488@gmail.com>
2024-08-16 16:12:08 -0400

    Update page.js

--- app/sign-out/[[...sign-out]]/page.js
+++ app/sign-out/[[...sign-out]]/page.js
@@ -8,32 +8,34 @@ import { SignedOut, SignIn } from '@clerk/nextjs';
 export default function SignOutPage() {
   return (
 	<>
-		<AppBar position="static" sx={{ backgroundColor: '#3f51b5' }}>
-			<Toolbar>
-			<Typography variant="h6" sx={{ flexGrow: 1 }}>
-				Flashcard SaaS
-			</Typography>
-			<Button color="inherit">
-				<Link href="/login" passHref>
-				<Typography component="span" sx={{ color: 'inherit', textDecoration: 'none' }}>
-					Sign Out
-				</Typography>
-				</Link>
-			</Button>
-			</Toolbar>
-		</AppBar>
-		<Box
-			display="flex"
-			flexDirection="column"
-			justifyContent="center"
-			alignItems="center"
-			sx={{ textAlign: 'center', my: 4 }}
-		>
-			<Typography variant="h4" component="h1" gutterBottom>
-			Sign Out
-			</Typography>
-			<SignIn />
-		</Box>
+	  <AppBar position="static" sx={{ backgroundColor: '#3f51b5' }}>
+		<Toolbar>
+		  <Typography variant="h6" sx={{ flexGrow: 1 }}>
+			Flashcard SaaS
+		  </Typography>
+		  <Button color="inherit">
+			<Link href="/login" passHref>
+			  <Typography component="span" sx={{ color: 'inherit', textDecoration: 'none' }}>
+				Sign Out
+			  </Typography>
+			</Link>
+		  </Button>
+		</Toolbar>
+	  </AppBar>
+	  <Box
+		display="flex"
+		flexDirection="column"
+		justifyContent="center"
+		alignItems="center"
+		sx={{ textAlign: 'center', my: 4 }}
+	  >
+		<Typography variant="h4" component="h1" gutterBottom>
+		  Sign Out
+		</Typography>
+		<SignedOut>
+		  <SignIn />
+		</SignedOut>
+	  </Box>
 	</>
   );
 }
