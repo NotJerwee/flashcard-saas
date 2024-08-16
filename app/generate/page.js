b834ec7a37a2c28feb4d4d5ccdf33f0f98194dd6
NotJerwee <jerrylin2488@gmail.com>
2024-08-15 23:32:07 -0400

    Update page.js

--- app/generate/page.js
+++ app/generate/page.js
@@ -132,6 +132,31 @@ export default function Generate() {
 
             </Box>
             )}
+
+            <Dialog open={dialogOpen} onClose={handleCloseDialog}>
+            <DialogTitle>Save Flashcard Set</DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                Please enter a name for your flashcard set.
+                </DialogContentText>
+                <TextField
+                autoFocus
+                margin="dense"
+                label="Set Name"
+                type="text"
+                fullWidth
+                value={setName}
+                onChange={(e) => setSetName(e.target.value)}
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleCloseDialog}>Cancel</Button>
+                <Button onClick={saveFlashcards} color="primary">
+                Save
+                </Button>
+            </DialogActions>
+            </Dialog>
+
         </Container>
     )
 }
