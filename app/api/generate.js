b4505e8dd43ca3b52dd16546083e8aee24f9b1fa
NotJerwee <jerrylin2488@gmail.com>
2024-08-17 21:43:57 -0400

    Update generate.js

--- app/api/generate.js
+++ app/api/generate.js
@@ -6,31 +6,31 @@ You are a flashcard creator, you take in text and create multiple flashcards fro
 Both front and back should be one sentence long.
 You should return in the following JSON format:
 {
-  "flashcards":[
-    {
-      "front": "Front of the card",
-      "back": "Back of the card"
-    }
-  ]
+	"flashcards":[
+		{
+			"front": "Front of the card",
+			"back": "Back of the card"
+		}
+	]
 }
 `
 
 export async function POST(req) {
-    const openai = new OpenAI()
-    const data = await req.text()
-  
-    const completion = await openai.chat.completions.create({
-      messages: [
-        { role: 'system', content: systemPrompt },
-        { role: 'user', content: data },
-      ],
-      model: 'gpt-4o',
-      response_format: { type: 'json_object' },
-    })
-  
-    // Parse the JSON response from the OpenAI API
-    const flashcards = JSON.parse(completion.choices[0].message.content)
-  
-    // Return the flashcards as a JSON response
-    return NextResponse.json(flashcards.flashcards)
-  }
+	const openai = new OpenAI()
+	const data = await req.text()
+
+	const completion = await openai.chat.completions.create({
+		messages: [
+			{ role: 'system', content: systemPrompt },
+			{ role: 'user', content: data },
+		],
+		model: 'gpt-4o',
+		response_format: { type: 'json_object' },
+	})
+	
+	// Parse the JSON response from the OpenAI API
+	const flashcards = JSON.parse(completion.choices[0].message.content)
+	
+	// Return the flashcards as a JSON response
+	return NextResponse.json(flashcards.flashcards)
+}
